a072b54b3a0e4ebb86de54b7bbf19f5459809983
Andrey Grebenshchikov <a-grebenshikov@bk.ru>
2023-01-27 03:37:11 +0500

    Добавлен механизм для удобного добавления ресурсов вне компонентной системы

--- Assets/_Common/Scripts/Engine/Resources/Components/ResourceComponent.cs
+++ Assets/_Common/Scripts/Engine/Resources/Components/ResourceComponent.cs
@@ -7,26 +7,22 @@ public class ResourceComponent<T> : GameComponent<T> where T : MiniGame
     [SerializeField] private int _minValue = 100;
     [SerializeField] private int _maxValue = 1000;
 
-    private Resource _resource;
+    private ResourceHolder _resourceHolder = new ResourceHolder();
 
-    public Resource Resource => _resource;
+    public Resource Resource => _resourceHolder != null ? _resourceHolder.Resource : null;
 
     protected override void OnInit()
     {
         base.OnInit();
 
-        _resource = MiniGame.EntryPoint.ResourcesManager.AddResource(MiniGame, GetTargetObject(), _name, _startValue, _minValue, _maxValue, GetChangePolicy());
+        _resourceHolder.Create(MiniGame, GetTargetObject(), _name, _startValue, _minValue, _maxValue, GetChangePolicy());
     }
 
     protected override void OnDeinit()
     {
         base.OnDeinit();
 
-        if (_resource != null)
-        {
-            MiniGame.EntryPoint.ResourcesManager.RemoveResource(_resource);
-            _resource = null;
-        }
+        _resourceHolder.Remove();
     }
 
     protected virtual ResourceChangePolicy GetChangePolicy() { return null; }
---
+++ Assets/_Common/Scripts/Engine/Resources/ResourceHolder.cs
@@ -0,0 +1,24 @@
+public class ResourceHolder
+{
+    private MiniGame _miniGame;
+    private Resource _resource;
+
+    public Resource Resource => _resource;
+
+    public void Create(MiniGame miniGame, string targetObject, string name, int startValue, int minValue, int maxValue, ResourceChangePolicy changePolicy)
+    {
+        _miniGame = miniGame;
+
+        _resource = _miniGame.EntryPoint.ResourcesManager.AddResource(miniGame, targetObject, name, startValue, minValue, maxValue, changePolicy);
+    }
+
+    public void Remove()
+    {
+        if (_resource != null)
+        {
+            _miniGame.EntryPoint.ResourcesManager.RemoveResource(_resource);
+
+            _resource = null;
+        }
+    }
+}
