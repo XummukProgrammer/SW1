d244a1706042c31e0f1ca056e0c968a9f259c237
Andrey Grebenshchikov <a-grebenshikov@bk.ru>
2023-02-19 00:54:15 +0500

    [Флоу постройки] Скрытие BuildingSelectHUD при старте и показ в конце

--- Assets/_Common/Scripts/Game/StrategyWar/MiniGame/Building/Actions/SW_BuildingItemAction.cs
+++ Assets/_Common/Scripts/Game/StrategyWar/MiniGame/Building/Actions/SW_BuildingItemAction.cs
@@ -1,7 +1,33 @@
 public class SW_BuildingItemAction : SequenceAction
 {
+    private SW_MiniGame _miniGame;
+
     public void InitActions(SW_MiniGame miniGame)
     {
-        AddAction(miniGame.BuildingSelectWindowComponent.Controller.GetOpenAction());
+        _miniGame = miniGame;
+
+        AddAction(GetHideBuildingSelectHUDAction());
+        AddAction(_miniGame.BuildingSelectWindowComponent.Controller.GetOpenAction());
+        AddAction(GetShowBuildingSelectHUDAction());
+    }
+
+    private Action GetHideBuildingSelectHUDAction()
+    {
+        var action = new Action();
+        action.ExecuteDelegate = () =>
+        {
+            _miniGame.BuildingSelectHUDComponent.Hide();
+        };
+        return action;
+    }
+
+    private Action GetShowBuildingSelectHUDAction()
+    {
+        var action = new Action();
+        action.ExecuteDelegate = () =>
+        {
+            _miniGame.BuildingSelectHUDComponent.Show();
+        };
+        return action;
     }
 }
